01afb61972c341aa939a64316151eb6828d09329
Xianfeng Zhuge <46865258+MengXinAlex@users.noreply.github.com>
2022-08-30 21:30:48 +1000

    update website

--- js/init.js
+++ js/init.js
@@ -514,7 +514,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Junior Developer", "Lifelong Learner", "Freelancer"],
+			strings: ["Software Developer", "Lifelong Learner", "Freelancer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
